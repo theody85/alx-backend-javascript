4f0104a0c7d8888717e7b49c96bca0149f97d528
theody85 <gyambrahtheodora.8@gmail.com>
2022-12-22 21:31:50 +0000

    Update file for task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise === Promise.resolve()) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else {
-      reject(Error());
-    }
-  }).then(console.log('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: 'sucess' }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
